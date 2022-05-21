bee011b6bdc85b635456a71544e7c5c3ee93e8b9
Wojtek <wojciechdebsqi@gmail.com>
2022-05-21 20:04:33 +0200

    Add Users with working CRUD

--- WorkTimeApi/Program.cs
+++ WorkTimeApi/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using WorkTimeApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<UserContext>(options =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("dev_db");
+        options.UseSqlServer(connectionString);
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,6 +21,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
