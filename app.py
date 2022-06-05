23283e673ee7d9d290a5265b91e9c2616d284578
Wojtek <wojciechdebsqi@gmail.com>
2022-06-05 11:38:53 +0200

    Add 'events' table and example query on main page

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from project import app, db
 from flask import render_template
-from project.models import User, Card
+from project.models import User, Card, Event
 import pandas as pd
-from sqlalchemy import select
 
 @app.route('/', methods=['GET'])
 def index():
-    query = select([
+    query = db.session.query(
+        Event.log_time,
+        Event.event_type,
+        Event.card_id,
+        Card.user_id,
         User.id,
         User.first_name,
-        User.last_name,
-        Card.id.label('card_id')
-    ]).join(Card, isouter=True)
-    df = pd.read_sql(query, db.engine)
+        User.last_name
+    ).select_from(Event).join(Card, full=True).join(User, full=True).order_by(Event.log_time.desc())
+    data = query.all()
+    df = pd.DataFrame(data)
+    
     return render_template('index.html', df=df)
 
 if __name__ == '__main__':
