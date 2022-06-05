45a0a97855e1cce1e7258f915bd04ecb3105405f
Wojtek <wojciechdebsqi@gmail.com>
2022-06-05 15:06:38 +0200

    Add /api/events endpoint with POST enabled

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from project import app, db
+from project import app, db, api
 from flask import render_template
+from flask_restful import Resource, reqparse
 from project.models import User, Card, Event
 import pandas as pd
+
+events_post_reqparse = reqparse.RequestParser()
+events_post_reqparse.add_argument('log_time', type=str, help='log_time is required', location='json', required=True)
+events_post_reqparse.add_argument('event_type', type=str, help='event_type is required', location='json', required=True)
+events_post_reqparse.add_argument('card_id', type=str, help='card_id is required', location='json', required=True)
+
+
+class EventApi(Resource):
+    def post(self):
+        args = events_post_reqparse.parse_args()
+        log_time = args['log_time']
+        event_type = args['event_type']
+        card_id = args['card_id']
+
+        new_event = Event(log_time=log_time, event_type=event_type, card_id=card_id)
+        db.session.add(new_event)
+        db.session.commit()
+
+        return {'message': 'new event added'}, 201
+
+api.add_resource(EventApi, '/api/events')
+
 
 @app.route('/', methods=['GET'])
 def index():
@@ -16,8 +39,9 @@ def index():
     ).select_from(Event).join(Card, full=True).join(User, full=True).order_by(Event.log_time.desc())
     data = query.all()
     df = pd.DataFrame(data)
-    
+
     return render_template('index.html', df=df)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
