b91bcaa6ecb54092f3d6276b0f842406841237a0
bfokss <foks.bartosz@gmail.com>
2022-06-14 22:05:51 +0200

    Add routing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 import pandas as pd
+
 
 from project import app, db
 from project.models import User, Card, Event
@@ -8,32 +9,46 @@ import project.api
 
 @app.route('/', methods=['GET'])
 def index():
-    query = db.session.query(
-        Event.log_time,
-        Event.event_type,
-        Event.card_id,
-        Card.user_id,
-        User.id,
-        User.first_name,
-        User.last_name
-    ).select_from(Event).join(Card, full=True).join(User, full=True).order_by(Event.log_time.desc())
-    data = query.all()
-    df = pd.DataFrame(data)
+    return render_template('home.html')
 
-    getUser = db.session.query(
-        User.first_name,
-    ).select_from(User).join(Card, full=True).order_by(Event.log_time.desc())
 
-    activeSession = False
+@app.route('/users', methods=['GET'])
+def user_index():
+    userId = request.args.get('id')
 
-    #userQuery = db.session.query(User).join(Card, full=True).order_by(Event.log_time.desc()).slice(1,2)
-    userData = query.first()
-    lastStartUser = query.filter(Event.event_type=='start')
-    print(lastStartUser)
-    #print(userData)
-    #print(activeSession)
+    userExists = db.session.query(User.id).filter_by(id=userId).first() is not None
 
-    return render_template('home/index.html', df=df, user=userData, activeSession=activeSession)
+    
+    if ('id' not in request.args) or (not userExists):
+        return redirect(url_for('index'))
+    
+    else:
+        query = db.session.query(
+            Event.log_time,
+            Event.event_type,
+            Event.card_id,
+            Card.user_id,
+            User.id,
+            User.first_name,
+            User.last_name
+        ).select_from(Event).join(Card, full=True).join(User, full=True).filter(User.id==userId).order_by(Event.log_time.desc())
+        data = query.all()
+        userData = pd.DataFrame(data)
+
+        trainings = db.session.query(Event.log_time, Event.event_type, Card.user_id, User.first_name).select_from(Event).join(Card, full=True).join(User, full=True).filter(User.id==userId,Event.event_type=="start").order_by(Event.log_time.desc()).all()
+        userTrainings = pd.DataFrame(trainings)
+        userTrainingsLimit = 10
+        print(trainings)
+
+        userFirstRow = query.first()
+
+        isTraining = userData['event_type'][0]=='start'
+
+        print(userData)
+
+        return render_template('main/index.html', userData=userData, userFirstRow=userFirstRow, isTraining=isTraining, userTrainings=userTrainings, userTrainingsLimit=userTrainingsLimit)
+
+
 
 
 if __name__ == '__main__':
