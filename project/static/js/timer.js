0a260deedcb25c257101b41b7b94b4a7e8fc38ee
bfokss <foks.bartosz@gmail.com>
2022-06-14 18:14:52 +0200

    Add timer

--- project/static/js/timer.js
+++ project/static/js/timer.js
@@ -4,11 +4,9 @@ function showTime(){
     var year = d.getFullYear();
     var month = d.getMonth();
     var date = d.getDate();
-    var day =d.getDay();
     var hour = d.getHours();
     var min = d.getMinutes();
     var sec = d.getSeconds();
-
 
 
     hour = ("0" + hour).slice(-2);
@@ -23,51 +21,37 @@ function showTime(){
         var trainingDay = trainingDateTime.getDate();
 
 
-        var diff = currentDateTime.getTime() - trainingDateTime.getTime();
+        var diff = Math.abs(currentDateTime.getTime() - trainingDateTime.getTime());
         var diffHours = Math.floor(diff / 1000 / 60 / 60);
         diff -= diffHours * 1000 * 60 * 60;
         var diffMinutes = Math.floor(diff / 1000 / 60);
 
-        if(diffHours <= 0){
-            diffHours = 24 + diffHours;
-        }
+        
 
         noclock.innerHTML = diffHours+':'+diffMinutes;
         trainingMonth += 1;
-        console.log(trainingMonth);
         tDate.innerHTML = trainingDay+'-'+trainingMonth+'-'+trainingYear;
         
     }
     else{
         month += 1;
-        clock.innerHTML = hour+':'+min+':'+sec;
+        clock.innerHTML = hour.padStart(2, '0')+':'+min.padStart(2, '0')+':'+sec.padStart(2, '0');
         cDate.innerHTML = date+'-'+month+'-'+year;
     }
-    
-    //console.log(d);
-    //console.log(trainingDateTime);
-    
-
 }
 
-noclock = document.getElementById("noclock");
-clock = document.getElementById("clock");
-tDate = document.getElementById("training_date");
-cDate = document.getElementById("date");
+var noclock = document.getElementById("noclock");
+var clock = document.getElementById("clock");
+var tDate = document.getElementById("training_date");
+var cDate = document.getElementById("date");
 
 
 if(noclock !== null){
-    var tYear = document.getElementById("year").innerHTML;
-    var tMonth = document.getElementById("month").innerHTML;
-    var tDay = document.getElementById("day").innerHTML;
-    var tHour = document.getElementById("hour").innerHTML;
-    var tMinute = document.getElementById("minute").innerHTML;
-    var tSecond = document.getElementById("second").innerHTML;
-    getTrainingDateTime = tYear+'-'+tMonth+'-'+tDay+' '+tHour+':'+tMinute+':'+tSecond;
-    trainingDateTime = new Date(getTrainingDateTime);
+    var tDateTime = document.getElementById("datetime").innerHTML;
+    trainingDateTime = new Date(tDateTime);
 }
 
-showTime()
+showTime();
 setInterval(showTime,1000);
         
         
